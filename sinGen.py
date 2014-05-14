8911cba9629d7395ac34e28127225621204a2839
Ralph Leyva <ralphfleyva@gmail.com>
2014-05-14 04:50:42 -0400

    This is a basic program that will generate a function that will later be used to test filters. The tester function will be used to put everything together

--- sinGen.py
+++ sinGen.py
@@ -18,22 +18,24 @@ class sinGen:
             self.sampleRate
             self.sinData = [(amplitude * numpy.sin((2 * numpy.pi) * freq * t) / sampleRate) for t in range(0, 2048)]
         else:
-            return TypeError("Function parameters are of the wrong type")
+            raise TypeError("Function parameters are of the wrong type")
 
     def sigSum(self, sin1):
-        if (type(sin1) == self.sinData):
-
+        if type(sin1) == self.sinData:
+            for i in range(0, self.sinData.__sizeof__()):
+                self.sinData[i] = self.sinData[i] + sin1.sinData[i]
         else:
-            return TypeError("Function parameters are of the wrong type")
+            raise TypeError("Function parameters are of the wrong type")
 
 
 class noiseGen:
     noiseData = numpy.array([])
 
-    def __init__(self, num)
+    def __init__(self, num, orig):
         #num corresponds to the numbers of signals that will be combined to generate a noise signal
         for i in range(0, num):
             amplitude = random.randrange(1, 20, 0.25)
             frequency = random.randrange(1000, 100000, 25)
             sampling = 44100
             x = sinGen(amplitude, frequency, sampling)
+            x.sigSum(x, orig)
