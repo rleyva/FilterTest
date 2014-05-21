7c7366058be95679678b7ab19fc90cd8ed4d9477
Ralph Leyva <ralphfleyva@gmail.com>
2014-05-21 14:27:49 -0400

    Remaking rtFFT_tester, it's way to slow to be practical, let alone useful. Pushing to git to have a backup of the changes that were last made

--- rtFFT_tester.py
+++ rtFFT_tester.py
@@ -1,10 +1,10 @@
 __author__ = 'Ralph Leyva'
 
 import pyaudio
-
-from matplotlib import pyplot
-
+import time
+from spectrum import *
 import rt_FFT
+from matplotlib import pyplot
 
 
 CHUNK = 1024
@@ -26,30 +26,50 @@ RATE = 44100
 # test = pyplot.plot(freqData.freq, freqData.fftb)
 # pyplot.show()
 
-test = pyplot
-test.ion()
+# test = pyplot
+# test.ion()
 
-while 1:
-    p = pyaudio.PyAudio()
-    strm = p.open(format=FORMAT,
-                  channels=CHANNELS,
-                  rate=RATE,
-                  input=True,
-                  frames_per_buffer=CHUNK)
+# This code currently works bt consumes way too mabny resources,
+# and is really, really, REALLY slow!!! Perhaps using the animation
+# libraries will fix this?
 
-    soundData = rt_FFT.soundRec(strm, 10)
-    freqData = rt_FFT.freqGen(RATE, soundData)
-
-    test.subplot(3, 1, 1)
-    test.plot(soundData)
-
-    test.subplot(3, 1, 2)
-    test.plot(freqData.freq, freqData.fftr)
-
-    test.subplot(3, 1, 3)
-    test.plot(freqData.freq, freqData.fftb)
-
-    test.draw()
-    test.clf()
-    strm.close()
-
+# THIS IS TRASH
+# p = pyaudio.PyAudio()
+# strm = p.open(format=FORMAT,
+#               channels=CHANNELS,
+#               rate=RATE,
+#               input=True,
+#               frames_per_buffer=CHUNK)
+#
+# while 1:
+#     tStart = time.time()
+#     p = pyaudio.PyAudio()
+#     strm = p.open(format=FORMAT,
+#                   channels=CHANNELS,
+#                   rate=RATE,
+#                   input=True,
+#                   frames_per_buffer=CHUNK)
+#     soundData = rt_FFT.soundRec(strm, 1)
+#     freqData = rt_FFT.freqGen(RATE, soundData)
+#
+#     test.subplot(3, 1, 1)
+#     test.plot(soundData)
+#
+#     test.subplot(3, 1, 2)
+#     test.plot(freqData.freq, freqData.fftr)
+#
+#     test.subplot(3, 1, 3)
+#     test.plot(freqData.freq, freqData.fftb)
+#
+#     test.draw(),
+#     print 'Time: ', time.time() - tStart, ' seconds'
+#     print 'Length of Data: ', len(freqData)
+#     test.clf()
+#     strm.close()
+# #
+# import matplotlib.pyplot as plt
+# import numpy
+#
+# hl, = plt.plot([], [])
+#
+#
